2c2bdff004607bea31d20131c0a1e9c573a2fa2b
mateus-dev-me <mateusmartinsipu2@gmail.com>
2023-06-14 13:40:57 -0300

    feat: init repo

---
+++ lua/settings.lua
@@ -0,0 +1,355 @@
+local g = vim.g
+local opt = vim.opt
+local cmd = vim.cmd
+
+
+-- Genaral setup
+opt.encodig =  'UTF-8'
+opt.compatible = false
+opt.mouse = 'a'
+opt.history = 50000
+opt.swapfile = true
+
+-- Editor
+opt.number = true
+opt.wrap = false
+opt.signcolumn = 'yes'
+opt.showmatch = true
+opt.showmode = false
+opt.foldmethod = 'marker'
+opt.splitright = true
+opt.splitbelow = true
+opt.conceallevel = 0
+opt.cursorline = true
+opt.scrolloff = 10
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.smartindent = true
+opt.list = true
+opt.shortmess:append { c = true }
+opt.whichwrap:append {
+	['<'] = true,
+	['>'] = true,
+	[','] = true,
+	h = true,
+	l = true
+}
+
+opt.incsearch = true
+opt.wildmenu = true
+opt.confirm = true
+
+opt.autoindent = true
+opt.smartindent = true
+
+-- Theme
+opt.termguicolors = true
+g.airline_theme ='dalton'
+
+
+-- Nvimtree configuration
+require('nvim-tree').setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+-- Terminal
+cmd [[command! Term :botright split term://$SHELL]]
+cmd [[
+  autocmd TermOpen * setlocal listchars= nonumber norelativenumber nocursorline
+  autocmd TermOpen * startinsert
+  autocmd BufLeave term://* stopinsert
+]]
+
+-- Floaterminal setup
+cmd [[
+  let g:floaterm_keymap_new = '<Leader>ft'
+  let g:floaterm_keymap_prev = '<Leader>fp'
+  let g:floaterm_keymap_next = '<Leader>fn'
+  let g:floaterm_keymap_toggle = '<Leader>t'
+  let g:floaterm_keymap_kill = '<Leader>fk'
+  let g:floaterm_title=' arch linux '
+]]
+
+-- Nvim Treesitter Configuration
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "vim", "help" },
+
+  sync_install = false,
+  auto_install = true,
+  ignore_install = { "javascript" },
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- lsp server configuration
+local has_any_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+require'lspconfig'.clangd.setup{}
+require "lsp_signature".setup()
+vim.o.completeopt = 'menuone,noselect'
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local cmp = require'cmp'
+local luasnip = require("luasnip")
+
+local lspkind = require('lspkind')
+local source_mapping = {
+  buffer = "◉ Buffer",
+  nvim_lsp = "👐 LSP",
+  nvim_lua = "🌙 Lua",
+  cmp_tabnine = "💡 Tabnine",
+  path = "🚧 Path",
+  luasnip = "🌜 LuaSnip"
+}
+
+cmp.setup({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+    { name = 'nvim_lua' },
+  },
+
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = lspkind.presets.default[vim_item.kind]
+      local menu = source_mapping[entry.source.name]
+      if entry.source.name == 'cmp_tabnine' then
+        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+          menu = entry.completion_item.data.detail .. ' ' .. menu
+        end
+        vim_item.kind = ''
+      end
+      vim_item.menu = menu
+      return vim_item
+    end
+  },
+
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+  },
+})
+
+require("luasnip/loaders/from_vscode").load()
+
+
+-- Tabnine configuration
+local has_any_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+require'lspconfig'.clangd.setup{}
+require "lsp_signature".setup()
+vim.o.completeopt = 'menuone,noselect'
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local cmp = require'cmp'
+local luasnip = require("luasnip")
+
+cmp.setup({
+  mapping = {
+
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end,
+
+  },
+
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  sources = {
+    { name = 'cmp_tabnine' },
+    { name = 'luasnip' },
+    { name = 'path' },
+  },
+})
+local tabnine = require('cmp_tabnine.config')
+tabnine:setup({
+  max_lines = 1000;
+  max_num_results = 20;
+  sort = true;
+  run_on_every_keystroke = true;
+  snippet_placeholder = '..';
+})
+
+require('lspkind').init({
+  with_text = true,
+  preset = 'codicons',
+  symbol_map = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "ﰠ",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "塞",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "פּ",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+  },
+})
+
+require("luasnip/loaders/from_vscode").load()
+
+local lspkind = require('lspkind')
+
+local source_mapping = {
+  buffer = "◉ Buffer",
+  nvim_lsp = "👐 LSP",
+  nvim_lua = "🌙 Lua",
+  cmp_tabnine = "💡 Tabnine",
+  path = "🚧 Path",
+  luasnip = "🌜 LuaSnip"
+}
+
+require'cmp'.setup {
+  sources = {
+    { name = 'cmp_tabnine' },
+    { name = 'luasnip' },
+    { name = 'path' }
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = lspkind.presets.default[vim_item.kind]
+      local menu = source_mapping[entry.source.name]
+      if entry.source.name == 'cmp_tabnine' then
+        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+          menu = entry.completion_item.data.detail .. ' ' .. menu
+        end
+        vim_item.kind = ''
+      end
+      vim_item.menu = menu
+      return vim_item
+    end
+  },
+}
